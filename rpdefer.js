cfc5aafc025041e37eec971f1331885be0799910
guprooon <guprooon@gmail.com>
2016-02-02 10:48:34 +0700

    Update and rename rpdefer.js to bp.js

--- rpdefer.js
+++
@@ -1,9 +0,0 @@
-var ca = function() {
-var i = document.createElement('script');
-i.href = 'https://cdn.rawgit.com/guprooon/Javascript/master/rp.js';
-var j = document.getElementsByTagName('article')[0]; j.parentNode.insertBefore(i, j);
-};
-var rbf = requestAnimationFrame || mozRequestAnimationFrame ||
-webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (rbf) rbf(ca);
-else window.addEventListener('load', ca);
