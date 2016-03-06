f6e199ca6cf727a28a8827813bb2f8f0df2b603d
guprooon <guprooon@gmail.com>
2016-03-06 13:34:01 +0700

    Update and rename ga.js to gaf.js

---
+++ gaf.js
@@ -0,0 +1,8 @@
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-50816265-1', 'auto');
+  ga('send', 'pageview');
+var cssLink=document.createElement("link");cssLink.href="https://cdn.rawgit.com/guprooon/Javascript/master/fonts.css";cssLink.rel="stylesheet";cssLink.type="text/css";
+document.head.appendChild(cssLink);
